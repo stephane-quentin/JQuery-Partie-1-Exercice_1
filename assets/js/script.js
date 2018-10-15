950aec17e84e475305383dbf9ba145093d8b5d4c
Stiouf <stephane.quentin@live.fr>
2018-10-15 15:02:37 +0200

    ex1

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(function() { // On utilise la méthode .click qui porte sur l'id (le bouton ici)
+  $('#hide').click(function(){ // Au clic, on utilise la méthode hide(cacher) pour cacher l'id correspondant (text)
+    $('#text').hide();
+    });
+});
